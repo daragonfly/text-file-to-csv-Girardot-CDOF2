53d67b0dca18f12eec15504501c23024393f673c
Arthur Girardot <arthur.girardot@edu.devinci.fr>
2025-01-13 14:19:14 +0100

    well import setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='text-file-to-csv-Girardot-CDOF2',
+    version='1.0',
+    author='daragonfly',
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  # Spécifie le type de contenu
+)
